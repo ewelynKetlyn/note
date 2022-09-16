15b73ea4d2ebaecf72a7aae6fb212283d231dd50
Ewelyn <ewelyn.silva@picpay.com>
2022-09-16 08:59:13 -0300

    novo botão para ir para nova tela

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.note
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.note.adapter.ItemAdapter
 import com.example.note.data.Datasource
 import com.example.note.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val myDataSet = Datasource().loadFrutas()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val buttonSegundaTela = findViewById<Button>(R.id.button_next)
+
         recyclerView.adapter = ItemAdapter(this, myDataSet)
         recyclerView.setHasFixedSize(true)
+
+        buttonSegundaTela.setOnClickListener{
+            IrSegundaTela()
+        }
+    }
+    private fun IrSegundaTela(){
+        val segundaTela = Intent(this, TestActivity::class.java)
+        startActivity(segundaTela)
     }
 }
